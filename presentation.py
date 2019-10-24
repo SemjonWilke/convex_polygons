8443342962bb13fe9d21c2a7d395a9f303b983d7
kernes92 <kernes92@zedat.fu-berlin.de>
2019-10-24 16:04:15 +0200

    purge V from E and add an example

--- presentation.py
+++ presentation.py
@@ -2,18 +2,15 @@ import matplotlib.pyplot as plt
 import json
 
 def readtestinstance(filename):
-    x = []
-    y = []
-    index = []
+    points = {'x': [], 'y': []}
     with open(filename) as json_file:
         data = json.load(json_file)
         for p in data['points']:
-            x.append(int(p['x']))
-            y.append(int(p['y']))
-            index.append(int(p['i']))
+            points['x'].append(int(p['x']))
+            points['y'].append(int(p['y']))
             # print('%i: (%.1f | %.1f)' % (int(p['i']), float(p['x']), float(p['y'])))
         instance = data['name']
-        return {'x': x, 'y': y, 'index' : index}, instance
+        return points, instance
 
 def writetestsolution(filename, instance, edges=[]):
     data = {
@@ -24,25 +21,35 @@ def writetestsolution(filename, instance, edges=[]):
     }
     for edge in edges:
         data['edges'].append({
-            'i': str(edge['in']['index']),
-            'j': str(edge['out']['index']),
+            'i': str(edge['in']),
+            'j': str(edge['out']),
         })
 
     with open(filename, 'w') as outfile:
         json.dump(data, outfile)
 
 def drawpoints(points, color='r.'):
-    for i in range(len(points['x'])):
+    for i,val in enumerate(points['x']):
         plt.plot(points['x'][i], points['y'][i], color)
 
-def drawedges(edges, color='b-'):
-    for edge in edges:
-        plt.plot((edge['in']['x'], edge['in']['y']),(edge['out']['x'],edge['out']['y']), color)
+def drawedges(edges, points, color='b-'):
+    for index,val in enumerate(edges['in']):
+        i = edges['in'][index]
+        j = edges['out'][index]
+        plt.plot(
+            [points['x'][i], points['x'][j]],
+            [points['y'][i], points['y'][j]],
+            color
+        )
 
 if __name__ == '__main__':
     points,instance = readtestinstance('euro-night-0000100.instance.json')
     drawpoints(points)
-    edges = { 'in' : {'x', 'y', 'index'}, 'out' :  {'x', 'y', 'index'}} #TODO x,y needn't be duplicated
-    drawedges(edges = [])  #TODO edges is empty
+    #''' delete # to switch comments and have an example
+    edges = {'in' : [], 'out' : []}
+    '''
+    edges = {'in' : [1,3,5,7,9], 'out' : [0,2,4,6,8]} # example
+    #'''
+    drawedges(edges,points)
     writetestsolution('euro-night-0000100.solution.json',instance)
     plt.show()
