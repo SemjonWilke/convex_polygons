e77f34ebc829a4c79fe7bd649a69dda352013dee
kernes92 <kernes92@zedat.fu-berlin.de>
2019-10-24 14:24:36 +0200

    add visualizer functions for V and E

--- presentation.py
+++ presentation.py
@@ -1,14 +1,31 @@
 import matplotlib.pyplot as plt
 import json
 
-if __name__ == '__main__':
-    x = [None]
-    y = [None]
+def readtestinstance(filename):
+    x = []
+    y = []
     with open('euro-night-0000100.instance.json') as json_file:
         data = json.load(json_file)
         for p in data['points']:
-            x.append(p['x'])
-            y.append(p['y'])
+            x.append(int(p['x']))
+            y.append(int(p['y']))
             # print('%i: (%.1f | %.1f)' % (int(p['i']), float(p['x']), float(p['y'])))
-    plt.plot(x,y, 'ro')
+        return [x,y]
+
+def writetestresult(filename):
+    return
+
+def drawpoints(points, color='r.'):
+    for i in range(len(points[0])):
+        plt.plot(points[0][i],points[1][i], color)
+
+def drawedges(edges, color='b-'):
+    for edge in edges:
+        plt.plot((edge[0][0], edge[0][1]),(edge[1][0],edge[1][1]), color)
+
+if __name__ == '__main__':
+    points = readtestinstance('euro-night-0000100.instance.json')
+    drawpoints(points)
+    #drawedges(edges)
+    #writetestresult()
     plt.show()
