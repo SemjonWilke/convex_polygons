1face0fbf14dc512f8e1d629e39f840a1157748b
kernes92 <kernes92@zedat.fu-berlin.de>
2019-10-24 14:48:54 +0200

    adapt json functions to specification

--- presentation.py
+++ presentation.py
@@ -4,16 +4,32 @@ import json
 def readtestinstance(filename):
     x = []
     y = []
-    with open('euro-night-0000100.instance.json') as json_file:
+    index = []
+    with open(filename) as json_file:
         data = json.load(json_file)
         for p in data['points']:
             x.append(int(p['x']))
             y.append(int(p['y']))
+            index.append(int(p['i']))
             # print('%i: (%.1f | %.1f)' % (int(p['i']), float(p['x']), float(p['y'])))
-        return [x,y]
+        instance = data['name']
+        return [x,y,index], instance
 
-def writetestresult(filename):
-    return
+def writetestsolution(filename, instance, edges=[]):
+    data = {
+        'type':'Solution',
+        'instance_name' : instance,
+        'meta' : {'comment':''},
+        'edges' : []
+    }
+    for edge in edges:
+        data['edges'].append({
+            'i': str(edge[0][2]),
+            'j': str(edge[1][2]),
+        })
+
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
 
 def drawpoints(points, color='r.'):
     for i in range(len(points[0])):
@@ -24,8 +40,8 @@ def drawedges(edges, color='b-'):
         plt.plot((edge[0][0], edge[0][1]),(edge[1][0],edge[1][1]), color)
 
 if __name__ == '__main__':
-    points = readtestinstance('euro-night-0000100.instance.json')
+    points,instance = readtestinstance('euro-night-0000100.instance.json')
     drawpoints(points)
     #drawedges(edges)
-    #writetestresult()
+    writetestsolution('euro-night-0000100.solution.json',instance)
     plt.show()
