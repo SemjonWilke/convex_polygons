6c2ef30880aa88834aba57cc4ffae537ed45ad56
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2019-10-30 09:58:29 +0100

    strip whitespaces from DCEL

--- DCEL.py
+++ DCEL.py
@@ -1,18 +1,12 @@
-
-
 import numpy as np
 from uuid import uuid4
 from collections import defaultdict
-
-
 
 class vertex:
     def __init__(self, x=None, y=None):
         self.x=x;       # floating
         self.y=y;       # floating
 
-
-        
 class edg_ind:
     def __init__(self, first=None, second=None):
         self.first=first;   # u-int 'origin vertex'
@@ -20,12 +14,11 @@ class edg_ind:
 
     def __eq__(self, _ind):
         return self.first == _ind.first and self.second == _ind.second
-   
+
     def __str__(self):
-        '''could be helpful for testing purposes''' 
+        '''could be helpful for testing purposes'''
         return "({0}, {1})".format(self.first,self.second)
 
-    
 class edge:
     def __init__(self,
             ind=None,         # edg_ind
@@ -33,8 +26,8 @@ class edge:
             incident_f=None,  # string 'face-id'
             prev=None,        # edg_ind
             _next=None        # edg_ind
-            ): 
-        
+            ):
+
         self.ind=ind                 # edg_ind
         self.orig=orig;              # u-int 'vertex'
         self.incident_f = incident_f # string 'face-id'
@@ -47,9 +40,6 @@ class edge:
         '''could be helpful for testing purposes'''
         return "Edge: {0} Orig: {1} InceF: {2} Prev: {3} Next: {4} ".format(self.ind,self.orig,self.incident_f,self.prev,self.next)
 
-
-
-
 class dcel_stuct:
 
     def __init__(self, data=None):
@@ -61,35 +51,31 @@ class dcel_stuct:
                 if self.data[_ind.first][i].ind == _ind:
                     return self.data[_ind.first][i]
         return None
-    
+
     def add_edg(self, _from, edg):
         if _from in self.data:
             self.data[_from] = np.append(self.data[_from], edg)
         else:
             self.data[_from] = np.array([edg])
 
-    
     def set_prev(self, _ind, prev):
         if _ind.first in self.data:
             for i in range(self.data[_ind.first].shape[0]):
                 if self.data[_ind.first][i].ind == _ind:
                     self.data[_ind.first][i].prev = prev
 
-        
     def set_next(self, _ind, _next):
         if _ind.first in self.data:
             for i in range(self.data[_ind.first].shape[0]):
                 if self.data[_ind.first][i].ind == _ind:
                     self.data[_ind.first][i].next = _next
 
-        
     def set_orig(self, _ind, orig):
         if _ind.first in self.data:
             for i in range(self.data[_ind.first].shape[0]):
                 if self.data[_ind.first][i].ind == _ind:
                     self.data[_ind.first][i].orig = orig
 
-        
     def set_incedent_f(self, _ind, incident_f):
         if _ind.first in self.data:
             for i in range(self.data[_ind.first].shape[0]):
@@ -111,11 +97,8 @@ class dcel_stuct:
                 if self.data[_ind.first][i].ind == _ind:
                     self.data[_ind.first] = np.delete(self.data[_ind.first], i)
                     return
-        
     # def key(self, _ind):
     #     return str(_ind.first)+str(_ind.second)
-
-
 
 class DCEL:
 
@@ -125,8 +108,6 @@ class DCEL:
         self.incidentFaces = defaultdict(set)  # maps vertices to face-IDs
         self.data = dcel_stuct()    # dcel entities
 
-
-        
     def sharedFaces(self, vert1, vert2):
         '''return a faces, that two vertices share, None if they don't share any'''
         if len(self.incidentFaces[vert1]) != 0 and len(self.incidentFaces[vert2]) != 0 :
@@ -135,7 +116,6 @@ class DCEL:
                 return None
             return shared_f # next(iter(shared_f))
 
-        
     def most_leftEdg(self,
                      face_1, # face-id
                      v1,      # u-int 'vertex'
@@ -163,9 +143,8 @@ class DCEL:
                 if _edg2.incident_f != face_1:            # to right
                     _edg2 = self.data.get_edg(edg_ind(v2, self.faces[face_1][i-1] ))
 
-        return _edg1, _edg2 
-    
-    
+        return _edg1, _edg2
+
     def split_face(self,
                    face_id, # face
                    _from,   # u-int 'vertex'
@@ -187,10 +166,9 @@ class DCEL:
                 x, y, z = np.split(self.faces[face_id], [ind2[0],ind1[0]])
                 r_face = np.append(z,np.append(x,y[0]))
                 l_face = np.append(y, z[0])
-                
-        return r_face, l_face       
-        
-    
+
+        return r_face, l_face
+
     def addEdg(self,
                _from,  # u-int 'vertex'
                _to,    # u-int 'vertex'
@@ -199,9 +177,9 @@ class DCEL:
 
         shared_f = self.sharedFaces(_from, _to)
         if shared_f == None:
-            # TODO 
+            # TODO
             print('dummy')
-            
+
         else:
             _e11, _e12 = self.most_leftEdg(shared_f, _from, _to)
             r_face, l_face = self.split_face( shared_f, _from, _to)
@@ -213,18 +191,18 @@ class DCEL:
                 self.incidentFaces[r_face[i]].add(rf_id)
             self.incidentFaces[r_face[0]].add(rf_id)
             self.incidentFaces[r_face[-1]].add(rf_id)
-            
+
             _e1 = None
             _e2 = None
             if _e11.ind.second == _from:  # right-to-left
-                _e1 = edge(edg_ind(_from, _to), _from, shared_f, _e11.ind, _e12.next)  
-                _e2 = edge(edg_ind(_to, _from), _to, rf_id, _e12.ind, _e12.prev)       
+                _e1 = edge(edg_ind(_from, _to), _from, shared_f, _e11.ind, _e12.next)
+                _e2 = edge(edg_ind(_to, _from), _to, rf_id, _e12.ind, _e12.prev)
                 self.data.add_edg(_from, _e1)    # add new edge
                 self.data.add_edg(_to, _e2)      # add twin of new edge
-                
+
                 temp = self.data(_e11.ind)
                 if temp != None:
-                    self.data.set_prev(temp.next, _e2.ind) 
+                    self.data.set_prev(temp.next, _e2.ind)
                     self.data.set_next(_e11.ind, _e1.ind)
 
                 temp = self.data(_e12.ind)
@@ -239,7 +217,7 @@ class DCEL:
                     temp = self.data(temp).next
 
             else:       # left-to-right
-                _e1 = edge(edg_ind(_to, _from), _to, shared_f, _e12.prev, _e11.ind)  
+                _e1 = edge(edg_ind(_to, _from), _to, shared_f, _e12.prev, _e11.ind)
                 _e2 = edge(edg_ind(_from, _to), _from, rf_id, _e11.prev, _e12.ind)
                 self.data.add_edg(_to, _e1)
                 self.data.add_edg(_from, _e2)
@@ -258,9 +236,8 @@ class DCEL:
                 temp = _e2.prev
                 while temp != _e12.ind:
                     self.data.set_incedent_f(temp, rf_id);
-                    temp = self.data(temp).prev  
+                    temp = self.data(temp).prev
 
-                
     def removeEdg(self,
                     _ind #  edg_ind
                     ):
@@ -276,7 +253,7 @@ class DCEL:
             face_2 = shared_f.pop()
             _merged = self.mergeSet(face_1, face_2, _ind.first, _ind.second)
             _twin = self.flip_edg(_ind)
-            
+
             if self.data(_ind).incident_f == face_1:
                 temp = self.data(self.data(_ind).next)
                 while temp.ind != _ind:
@@ -304,18 +281,16 @@ class DCEL:
             self.data.remove_edg(_ind)
             self.data.remove_edg(_twin)
 
-
-
     def flip_edg(self, _ind):
         return edg_ind(_ind.second, _ind.first)
-        
+
     def addFace(self, verts):
         '''add a face from an array of vertices'''
         face_id = uuid4();
         self.faces[face_id] = verts
         for i in range(verts.shape):
-            self.incidentFaces[verts[i]].add(face_id)    
-        
+            self.incidentFaces[verts[i]].add(face_id)
+
     def addVert(self, vert):  # this should not be needed, as the num of vertices do not change!
         self.vertices = np.append(self.vertices, vert)
 
@@ -326,7 +301,7 @@ class DCEL:
             ):
         '''check if the vertex '_vert' is on the left of line going through vertices '_from' and '_to'  '''
         return ((self.vertices[_to].x - self.vertices[_from].x)
-                    *(self.vertices[_vert].y - self.vertices[_from].y) 
+                    *(self.vertices[_vert].y - self.vertices[_from].y)
                     - (self.vertices[_to].y - self.vertices[_from].y)
                     *(self.vertices[_vert].x - self.vertices[_from].x)) <= 0;
 
@@ -336,14 +311,13 @@ class DCEL:
         ):
         '''check if the vertex 'vert' is on the left of edge 'edg'  '''
         return self.v_isLeftTo(self.data(edg).orig,
-                          self.data(self.data(edg).next).orig, 
+                          self.data(self.data(edg).next).orig,
                           vert);
 
-                          
-    def mergeSet(self, 
+    def mergeSet(self,
             a,   # face-id
             b,   # face-id
-            f,   # u-int "vertex, the origin of edge" 
+            f,   # u-int "vertex, the origin of edge"
             l    # u-int "vertex, destination of edge"
             ):
         '''merge two faces that consist of two arrays of vertices into one at a place of a given edge'''
@@ -463,6 +437,5 @@ class DCEL:
                 x1, y1, z1 = np.split(a, [l1[0], f1[0]])
                 x2, y2, z2 = np.split(b, [f2[0], l2[0]])
                 _merged = np.append(y1, y2)
-    
-        return _merged
 
+        return _merged
