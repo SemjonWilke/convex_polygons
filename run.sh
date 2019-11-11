672bbcff066bfcf66dd170245f1858a26c4a446f
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2019-11-11 13:47:12 +0100

    todo list for shell file

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo hi
+#repeat until stopped and iterate over i (as seed):
+#   for each file fp in instances/*/ do:
+#       execute "python3 presentation.py instances/*/fp -r i -o"
