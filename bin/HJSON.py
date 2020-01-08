f0d16e315b81ef8c297cc5658c787a75914657e8
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2020-01-08 10:30:09 +0100

    bin/HJSON: use float for points

--- bin/HJSON.py
+++ bin/HJSON.py
@@ -108,7 +108,7 @@ def readTestInstance(filename):
     with open(filename) as json_file:
         data = json.load(json_file)
         for p in data['points']:
-            points.append([int(p['x']), int(p['y'])]) # TODO does not work for float
+            points.append([float(p['x']), float(p['y'])]) # TODO does not work for float
         instance = data['name']
         json_file.close()
         return points, instance
