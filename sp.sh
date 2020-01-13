982da6c1424acaf08421fea4a948efdaccfdc1ab
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2020-01-13 11:45:31 +0100

    topostart: cleanup script

--- sp.sh
+++ sp.sh
@@ -2,20 +2,5 @@
 
 for f in instances/*/*.instance.json ; do
     echo -en "\r\b\b$f\033[0K"
-    python3 topo_start.py $f -s 50;
-done;
-
-for f in instances/*/*.instance.json ; do
-    echo -en "\r\b\b$f\033[0K"
-    python3 topo_start.py $f -s 75;
-done;
-
-for f in instances/*/*.instance.json ; do
-    echo -en "\r\b\b$f\033[0K"
-    python3 topo_start.py $f -s 100;
-done;
-
-for f in instances/*/*.instance.json ; do
-    echo -en "\r\b\b$f\033[0K"
-    python3 topo_start.py $f -s 200;
+    python3 topo_start.py $f;
 done;
