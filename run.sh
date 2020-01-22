d2f4f0869792533c6132717170437db977026bbe
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2020-01-22 13:34:23 +0100

    run: execute on second batch

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 COUNTER=1;
 while [ 1 ] ; do
     echo -e "\n$COUNTER"
-    for f in instances/*/*.instance.json ; do
+    for f in instances/*_*/*.instance.json ; do
         echo -en "\r\b\b$f\033[0K"
-        python3 presentation.py $f -r $COUNTER -o;
-        python3 checker.py $f;
+        python3 presentation.py $f -r $COUNTER -o -a ben_v1;
+        if [ $? -eq 0 ]
+            then python3 checker.py $f;
+        fi
         if [ $? -eq 3 ]
             then exit 1
         fi
