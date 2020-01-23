7e856470e7d621ea7ed0f2daaa27643126fa113e
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2020-01-23 12:43:34 +0100

    run: ben_v1 on batch of doom

--- run.sh
+++ run.sh
@@ -4,10 +4,7 @@ while [ 1 ] ; do
     echo -e "\n$COUNTER"
     for f in instances/*_*/*.instance.json ; do
         echo -en "\r\b\b$f\033[0K"
-        python3 presentation.py $f -r $COUNTER -o -a ben_v1;
-        if [ $? -eq 0 ]
-            then python3 checker.py $f;
-        fi
+        python3 presentation.py $f -r $COUNTER -o -a ben_v1 2> /dev/null;
         if [ $? -eq 3 ]
             then exit 1
         fi
