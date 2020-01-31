dd7ed46a87ae2803ea9002d65e6502003baaa273
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2020-01-31 18:07:02 +0100

    run script for abbas

---
+++ run_abbas.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+COUNTER=1000;
+LIMIT=1000;
+while [ 1 ] ; do
+    echo -e "\n$(( $COUNTER+1 )) $(( $COUNTER+$LIMIT ))"
+    for f in instances/*/*.instance.json; do
+        echo -en "\r\b\b$f\033[0K"
+        python3 presentation.py -l $(( $COUNTER+1 )) $(( $COUNTER+$LIMIT )) $f -a abbas -o -v;
+    done;
+
+    COUNTER=$(( $COUNTER+$LIMIT ));
+    CHECK=$(( $LIMIT*10 ));
+    if [ $COUNTER -eq $CHECK ];
+    then
+        LIMIT=$CHECK;
+        sleep 1
+    fi;
+done
