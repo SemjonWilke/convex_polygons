c7840965a15dac4d97fe3c962aa14f6212044ec4
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2020-01-31 21:15:41 +0100

    run script faster

--- run_v3.sh
+++ run_v3.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-COUNTER=0;
-LIMIT=10;
+COUNTER=10;
 while [ 1 ] ; do
-    echo -e "\n$(( $COUNTER+1 )) $(( $COUNTER+$LIMIT ))"
+    echo -e "\n$(( $COUNTER )) $(( $COUNTER*10-1 ))"
     for f in instances/*/*.instance.json; do
         echo -en "\r\b\b$f\033[0K"
-        python3 presentation.py -l $(( $COUNTER+1 )) $(( $COUNTER+$LIMIT )) $f -a ben_v3 -o -e 33;
+        python3 presentation.py -l $(( $COUNTER )) $(( $COUNTER*10-1 )) $f -a ben_v3 -o -e 33 -v;
     done;
 
-    COUNTER=$(( $COUNTER+$LIMIT ));
-    CHECK=$(( $LIMIT*10 ));
-    if [ $COUNTER -eq $CHECK ];
-    then
-        LIMIT=$CHECK;
-    fi;
+    COUNTER=$(( $COUNTER*10 ));
 done
