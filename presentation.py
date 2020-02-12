8741876796cfe7e464ae98c02ccc7d564490010f
Semjon Kerner <Semjon.Kerner@fu-berlin.de>
2020-02-12 11:51:10 +0100

    rename algorithms from working title

--- presentation.py
+++ presentation.py
@@ -14,10 +14,10 @@ import HDCEL
 import HJSON
 import HVIS
 
-import algorithm_ben_v1
-import algorithm_ben_v2
-import algorithm_ben_v3
-import algorithm_abbas
+import algorithm_wave
+import algorithm_merged
+import algorithm_pass
+import algorithm_nested
 
 def exithandler(sig, frame):
     print('Exiting')
@@ -27,17 +27,17 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, exithandler)
     try:
         # parser --------------------------------------------------------------
-        parser = argparse.ArgumentParser(description='Bens Algorithm for SoCG')
+        parser = argparse.ArgumentParser(description='Algorithms for SoCG 2020 by students of FU Berlin')
         parser.add_argument('file')
-        group = parser.add_mutually_exclusive_group()
-        group.add_argument('-c', '--coordinates', type=int, nargs=2, dest='coord', help='Coordinates of starting point')
-        group.add_argument('-r', '--random', nargs='?', default=0, dest='rndm', help='Use random starting point with optional seed')
+        parser.add_argument('-a', '--algorithm', type=str, dest='algorithm', default="pass",
+                            choices=["wave", "merged", "pass", "nested"], help='choose algorithm to execute')
         parser.add_argument('-o', '--overwrite', action='store_true', dest='overwrite', help='Overwrite existing solution if better')
-        parser.add_argument('-p', '--plot', action='store_true', dest='plot', help='Show plot')
-        parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Print human readable information')
-        parser.add_argument('-a', '--algorithm', type=str, dest='algorithm', default="ben_v1", choices=["ben_v1", "ben_v2", "ben_v3", "abbas"], help='choose algorithm to execute')
-        parser.add_argument('-e', '--explicit', type=int, default=0, dest='explicit', help='Amount of explicit starting points to take from startpoints argument')
-        group.add_argument('-l', '--limit', type=int, nargs=2, dest='limit', help='Dont execute if amount of points is outside of limit')
+        parser.add_argument('-p', '--plot', action='store_true', dest='plot', help='Show plot - not recommended for large instances')
+        parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Print some information')
+        parser.add_argument('-l', '--limit', type=int, nargs=2, dest='limit', help='Don\'t execute if amount of points is outside of limit')
+        parser.add_argument('-c', '--coordinates', type=int, nargs=2, dest='coord', help='Set coordinates of start point')
+        parser.add_argument('-r', '--random', nargs='?', default=0, dest='rndm', help='Set random seed')
+        parser.add_argument('-e', '--explicit', type=int, default=0, dest='explicit', help='Amount of used start points in percent')
         arg = parser.parse_args()
 
         # init ----------------------------------------------------------------
@@ -54,27 +54,22 @@ if __name__ == '__main__':
         HDCEL.points = points
         vertices = [HDCEL.Vertex(index=i) for i in range(len(points))]
 
-        if arg.limit != None:
-            if len(points) < arg.limit[0] or len(points) > arg.limit[1]:
-                if arg.verbose: print("Error: outside of limit %d < %d < %d" % (arg.limit[0], len(points), arg.limit[1]))
-                exit(2)
-
         c = arg.coord
         if arg.coord == None:
             c = HCOMMON.randomstart(points, arg.rndm, arg.verbose)
 
         # run -----------------------------------------------------------------
-        if arg.algorithm == "ben_v1":
-            algorithm_ben_v1.run(_vertices=vertices, _origin=HDCEL.Vertex(explicit_x=c[0], explicit_y=c[1]), _verbose=arg.verbose)
+        if arg.algorithm == "wave":
+            algorithm_wave.run(_vertices=vertices, _origin=HDCEL.Vertex(explicit_x=c[0], explicit_y=c[1]), _verbose=arg.verbose)
 
-        if arg.algorithm == "ben_v2":
-            algorithm_ben_v2.run(_vertices=vertices, _filename=arg.file, _verbose=arg.verbose, _explicit=arg.explicit)
+        elif arg.algorithm == "merged":
+            algorithm_merged.run(_vertices=vertices, _filename=arg.file, _verbose=arg.verbose, _explicit=arg.explicit)
 
-        if arg.algorithm == "ben_v3":
-            algorithm_ben_v3.run(_vertices=vertices, _filename=arg.file, _verbose=arg.verbose, _explicit=arg.explicit)
+        elif arg.algorithm == "pass":
+            algorithm_pass.run(_vertices=vertices, _filename=arg.file, _verbose=arg.verbose, _explicit=arg.explicit)
 
-        if arg.algorithm == "abbas":
-            algorithm_abbas.run(arg.verbose, vertices)
+        elif arg.algorithm == "nested":
+            algorithm_nested.run(arg.verbose, vertices)
 
         # cleanup -------------------------------------------------------------
         edges = HDCEL.get_edge_dict(arg.verbose)
